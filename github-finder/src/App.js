03553d40819f56186ff0126c0a459c13670f781e
hgusinac <hgusinac@gmail.com>
2022-02-01 23:15:31 +0100

    Components & Pages routing added

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Notfound from "./pages/Notfound";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<Notfound />} />
+            <Route path="/*" element={<Notfound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ github-finder/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function home() {
+  return (
+    <div>
+      <h1 className="tex-6xl">Welcome</h1>
+    </div>
+  );
+}
+
+export default home;
